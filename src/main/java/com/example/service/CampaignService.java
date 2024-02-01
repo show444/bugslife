804746d84b05a61a1ac24dac3b7ccd80c1151728
show444 <156166487+show444@users.noreply.github.com>
2024-02-01 19:46:35 +0900

    No.21 ステータス一括更新エラー時の処理。

--- src/main/java/com/example/service/CampaignService.java
+++ src/main/java/com/example/service/CampaignService.java
@@ -117,11 +117,11 @@ public class CampaignService {
 	 * @param nexStatus 更新後ステータス
 	 * @throws Exception
 	 */
-	@Transactional(readOnly = false, rollbackFor = RuntimeException.class)
+	@Transactional(readOnly = false, rollbackFor = Exception.class)
 	public void bulkStatusUpdate(List<Long> idList, CampaignStatus nexStatus) throws Exception {
 		try {
 			idList.forEach(id -> {
-				Campaign campaign = campaignRepository.findById(id).get();
+				Campaign campaign = campaignRepository.findById(id).orElseThrow(() -> new RuntimeException("Campaign not found for id: " + id));
 				// 更新前後のステータスが同じ場合はエラー
 				if (nexStatus.getId() == campaign.getStatus().getId()) {
 					throw new RuntimeException(campaign.getName() + "にステータスの変更がないため、ステータスの一括更新に失敗しました。");
